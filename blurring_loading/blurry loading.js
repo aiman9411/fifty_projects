7dcce70c112e0975d287ec04c1b5a94529e92aec
Aiman Nazmi <aimannazmi59@gmail.com>
2021-10-24 01:41:30 +0800

    loading blurry

--- blurring_loading/blurry loading.js
+++ blurring_loading/blurry loading.js
@@ -1,15 +1,22 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
-let load = 0
+const loadText = document.querySelector('.loading-text');
+const background = document.querySelector('.bg');
 
-let int = setInterval(blurring, 30)
+let load = 0;
+
+let int = setInterval(blurring, 50);
 
 function blurring() {
-  load++
+  load++;
 
-  if (load > 99) {
-    clearInterval(int)
-  }
+  if(load > 99) {
+    clearInterval(int);
+  };
 
   loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
 }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+};
+
