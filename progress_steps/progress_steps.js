087a3c3a71eedef0b837dc894fcc5da527450fee
Aiman Nazmi <aimannazmi59@gmail.com>
2021-11-11 23:37:48 +0800

    self update

--- progress_steps/progress_steps.js
+++ progress_steps/progress_steps.js
@@ -1,16 +1,16 @@
 // Query everything
-const progress = document.getElementById('progress');
-const next = document.getElementById('next');
 const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
+const progress = document.getElementById('progress');
 
 // Start the counter
 let activeNum = 1;
 
 // Work on Next button
 next.addEventListener('click', () => {
-    activeNum++;
 
+    activeNum++
     if(activeNum > circles.length) {
         activeNum === circles.length
     }
@@ -20,8 +20,7 @@ next.addEventListener('click', () => {
 
 // Work on Prev button
 prev.addEventListener('click', () => {
-    activeNum--;
-
+    activeNum--
     if(activeNum < 1) {
         activeNum === 1
     }
@@ -32,23 +31,21 @@ prev.addEventListener('click', () => {
 // Create Update Function
 function update() {
     circles.forEach((circle, idx) => {
-        if(idx < activeNum) {
+        if(activeNum > idx) {
             circle.classList.add('active')
         } else {
-            circle.classList.remove('active')
+            circle.classList.remove('active');
         }
     })
-    
-    const actives = document.querySelectorAll('.active');
-    progress.style.width = ((actives.length - 1)/(circles.length - 1))*100 + '%';
 
-    if(activeNum > circles.length) {
+    progress.style.width = ((activeNum - 1)/(circles.length - 1))*100 + '%';
+
+    if(activeNum === circles.length) {
         next.disabled = true;
-    } else if (activeNum < 1) {
+    } else if (activeNum === 1) {
         prev.disabled = true;
     } else {
         next.disabled = false;
         prev.disabled = false;
     }
-
 }
