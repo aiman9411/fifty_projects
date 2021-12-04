30768e819084a99179024295b911975df619057b
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-04 11:59:39 +0800

    add random select function

--- random_picker2/randpm_picker2.js
+++ random_picker2/randpm_picker2.js
@@ -5,7 +5,14 @@ textArea.focus();
 
 textArea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
-    // console.log(e.target.value);
+    
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10);
+
+        randomSelect();
+    }
 })
 
 function createTags(input) {
@@ -19,4 +26,30 @@ function createTags(input) {
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+}
+
+function randomSelect() {
+    const times = 30;
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100);
+    }, 100);
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
 }
