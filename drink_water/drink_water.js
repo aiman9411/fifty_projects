3a1057a22a84fd9f000e95f68018f2f3ad3fcc5d
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-11 10:11:26 +0800

    modify highlight cups function

--- drink_water/drink_water.js
+++ drink_water/drink_water.js
@@ -9,6 +9,11 @@ smallCups.forEach((cup, idx) => {
 
 function highlightCups(idx) {
     smallCups.forEach((cup, idx2) => {
+
+        if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+            idx--
+        }
+
         if(idx2 <= idx) {
             cup.classList.add('full');
         } else {
