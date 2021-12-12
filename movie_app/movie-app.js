ab1a31ee6c69ae3f4cc3442931344fa6b987b097
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-12 13:17:47 +0800

    delete "

--- movie_app/movie-app.js
+++ movie_app/movie-app.js
@@ -1,7 +1,7 @@
 //Define API variables
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6b2d2f00a8d1c438c197ea4c44eede8';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=b6b2d2f00a8d1c438c197ea4c44eede8&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=b6b2d2f00a8d1c438c197ea4c44eede8&query=';
 
 //Create element via query
 const form = document.getElementById('form');
