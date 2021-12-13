4723db63259a81b7fc903cdd8602d73da0915534
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-13 10:19:28 +0800

    end

--- movie_app/movie-app.js
+++ movie_app/movie-app.js
@@ -1,5 +1,5 @@
 //Define API variables
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc&api_key=b6b2d2f00a8d1c438c197ea4c44eede8';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6b2d2f00a8d1c438c197ea4c44eede8&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=b6b2d2f00a8d1c438c197ea4c44eede8&query=';
 
@@ -15,6 +15,7 @@ getMovies(API_URL);
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
+    // console.log(data.results);
     showMovies(data.results);
 }
 
