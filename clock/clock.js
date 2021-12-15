21ff82a1fe87126b74b8793ba18d4f97098a11c5
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-15 15:47:03 +0800

    first try

--- clock/clock.js
+++ clock/clock.js
@@ -1,42 +1,43 @@
 const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
-const timeEl = document.querySelector('.time');
-const dateEl = document.querySelector('.date');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('HTML');
+    const html = document.querySelector('html');
     if(html.classList.contains('dark')) {
         html.classList.remove('dark');
-        e.target.innerHTML = 'Dark Mode'
+        toggle.innerHTML = 'Dark Mode'
     } else {
         html.classList.add('dark');
-        e.target.innerHTML= 'Light Mode'
+        toggle.innerHTML = 'Light Mode'
     }
 })
 
+
 function setTime() {
-    const time = new Date();
-    const month = time.getMonth();
-    const day = time.getDay();
-    const date = time.getDate();
-    const hours = time.getHours();
-    const hoursForClock = hours % 12;
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-    const ampm = hours >= 12 ?'PM':'AM';
+    const currentDate = new Date();
+    const hour = currentDate.getHours();
+    const hoursForClock = hour % 12;
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const ampm = hour >= 12 ? 'PM': 'AM';
+    const day = currentDate.getDay();
+    const month = currentDate.getMonth();
+    const dates = currentDate.getDate();
+    // console.log(day);
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes} ${ampm}`;
-    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
-
+    time.innerHTML = `${hoursForClock}:${minutes < 10? `0${minutes}`: minutes} ${ampm}`
+    date.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${dates}</span>`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
