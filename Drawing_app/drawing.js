3ecdccdb58916796a07f8d9cad4f984255ee208a
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-21 23:18:44 +0800

    add drawcircle function

---
+++ Drawing_app/drawing.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+let size = 20;
+
+let color = 'black';
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0,Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill()
+}
+
+drawCircle(320, 10);
