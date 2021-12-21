e204aab73d66318c186ec2d3a6adb326895bef8a
Aiman Nazmi <aimannazmi59@gmail.com>
2021-12-21 23:25:12 +0800

    add draw line function

--- Drawing_app/drawing.js
+++ Drawing_app/drawing.js
@@ -5,6 +5,9 @@ let size = 20;
 
 let color = 'black';
 
+let x;
+let y;
+
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0,Math.PI * 2);
@@ -12,4 +15,12 @@ function drawCircle(x, y) {
     ctx.fill()
 }
 
-drawCircle(320, 10);
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke()
+}
+
