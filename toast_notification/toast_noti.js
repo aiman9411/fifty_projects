f5f9ca73a79ba684407e979dffbecc51dd319322
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-01 16:11:06 +0800

    add random color function

--- toast_notification/toast_noti.js
+++ toast_notification/toast_noti.js
@@ -2,14 +2,16 @@ const button = document.querySelector('button');
 const toasts = document.querySelector('.toasts');
 
 const message = ['Message 1', 'Message 2', 'Message 3'];
+const color = ['red', 'blue', 'green']
 
 button.addEventListener('click', () => {
     const div = document.createElement('div');
     div.classList.add('toast');
+    div.style.color = color[Math.floor(Math.random() * color.length)];
     div.innerText = message[Math.floor(Math.random() * message.length)];
     toasts.appendChild(div);
 
     setTimeout(() => {
         div.remove()
-    }, 2000);
+    }, 3000);
 })
