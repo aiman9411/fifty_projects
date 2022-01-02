6502dc950d6e30b95c6b2e0f0826b45358df0af0
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-02 11:38:46 +0800

    Change e.target.innerHTML

--- github_card/github_card.js
+++ github_card/github_card.js
@@ -1,10 +1,12 @@
 const toggle = document.getElementById('mode');
 
-toggle.addEventListener('click', () => {
+toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html');
     if(html.classList.contains('dark')) {
         html.classList.remove('dark')
+        e.target.innerHTML = 'Dark Mode'
     } else {
         html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
     }
 })
