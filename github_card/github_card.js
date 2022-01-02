3aadae1fc24e50b06401eb4f3d12fbb3743ac294
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-02 18:52:42 +0800

    add getUser function

--- github_card/github_card.js
+++ github_card/github_card.js
@@ -10,3 +10,18 @@ toggle.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light Mode'
     }
 })
+
+const APIURL = 'https://api.github.com/users/'
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+getUser('aiman');
+
+async function getUser(username) {
+    try {
+        const {data} = await axios(APIURL + 'username')
+        console.log(data)
+    } catch(err) {
+        console.log(err.message);
+    }
+}
