f537cea3878db273d46c321b2b3aafdc2507ffd5
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-08 23:31:45 +0800

    add 'add sentence' function

--- auto_text/auto_text.js
+++ auto_text/auto_text.js
@@ -1,7 +1,10 @@
 const statementEl = document.getElementById('statement');
 const speedEl = document.getElementById('speed');
-const text = "My name is Aiman";
+const sentenceEl = document.getElementById('sentence');
+const form = document.getElementById('form');
+let text = "My name is Aiman"; 
 let idx = 1;
+let speed = 300/speedEl.value;
 
 genStatement();
 
@@ -13,6 +16,14 @@ function genStatement() {
         idx = 1;
     }
 
-    setTimeout(genStatement, 300);
+    setTimeout(genStatement, speed);
 }
 
+speedEl.addEventListener('input', (e) => speed = 300/e.target.value);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    text = sentenceEl.value;
+    sentenceEl.value = '';
+})
