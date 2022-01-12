4d59c3860a4d1580b915d61f2f41d9d6a2c6348a
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-12 23:50:25 +0800

    define elements

--- password_gen/password_gen.js
+++ password_gen/password_gen.js
@@ -1,3 +1,12 @@
+const resultEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('gen-button');
+const copyEl = document.getElementById('copy-button');
+
 const randomFunc = {
     lower: getLower,
     upper: getUpper,
