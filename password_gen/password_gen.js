8837d6912c21218827c79ee817cf9b95b06ecab1
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-14 10:52:37 +0800

    completed generatePassword function

--- password_gen/password_gen.js
+++ password_gen/password_gen.js
@@ -7,30 +7,6 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('gen-button');
 const copyEl = document.getElementById('copy-button');
 
-
-generateEl.addEventListener('click', () => {
-    const length = +lengthEl.value;
-    const hasUpper = uppercaseEl.checked;
-    const hasLower = lowercaseEl.checked;
-    const hasNumber = numbersEl.checked;
-    const hasSymbol = symbolsEl.checked; 
-
-    resultEl.innerText = generatePassword(hasUpper, hasLower, 
-        hasNumber, hasSymbol, length);
-
-})
-
-function generatePassword(upper, lower, number, symbol, length) {
-    let generatedPassword = '';
-    const typeCount = upper + lower + number + symbol;
-    const typesArr = [{upper}, {lower}, {number}, {symbol}].
-    filter(item => Object.values(item)[0])
-    
-    if(typeCount === 0) {
-        return '';
-    }
-}
-
 const randomFunc = {
     lower: getLower,
     upper: getUpper,
@@ -39,16 +15,55 @@ const randomFunc = {
 }
 
 
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasUpper = uppercaseEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    // console.log(length, hasUpper, hasLower, hasNumber, hasSymbol);
+
+    resultEl.innerText = generatePassword(hasUpper, hasLower, 
+        hasNumber, hasSymbol, length);
+
+})
+
+function generatePassword(upper, lower, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = upper + lower + number + symbol;
+    // console.log(typesCount);
+    const typesArr = [{upper}, {lower}, {number}, {symbol}].
+    filter(item => Object.values(item)[0])
+    // console.log(typesArr);
+    
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            // console.log(funcName);
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    // console.log(generatedPassword);
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
 function getLower() {
-    return String.fromCharCode(Math.floor(Math.random * 26) + 97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 };
 
 function getUpper() {
-    return String.fromCharCode(Math.floor(Math.random * 26) + 65)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 };
 
 function getNumber() {
-    return String.fromCharCode(Math.floor(Math.random * 10) + 48)
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 };
 
 function getSymbol() {
