8bc55e08214b6095e0bba9e3e8b97a175dee4126
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-15 17:00:02 +0800

    add answer section

---
+++ good_cheap/good_cheap.js
@@ -0,0 +1,26 @@
+const toggles = document.querySelectorAll('.toggle');
+const decentralised = document.querySelector('#decentralised');
+const scalable = document.querySelector('#scalable');
+const secured = document.querySelector('#secured');
+const answer = document.getElementById('answer');
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)));
+
+function doTheTrick(theClickedOne) {
+    if(decentralised.checked && scalable.checked && secured.checked) {
+        if(decentralised == theClickedOne) {
+            secured.checked = false;
+            answer.innerText = 'Multi-chain ecosystems';
+        }
+
+        if(scalable == theClickedOne) {
+            decentralised.checked = false;
+            answer.innerText = 'High-TPS chains';
+        }
+
+        if(secured == theClickedOne) {
+            scalable.checked = false;
+            answer.innerText = 'Traditional blockchains';
+        }
+    }
+}
