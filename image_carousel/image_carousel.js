b6953aad2f2b0919e7b4d4aedd4edb5e8669858b
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-24 11:20:22 +0800

    add button functionality

--- image_carousel/image_carousel.js
+++ image_carousel/image_carousel.js
@@ -1,5 +1,7 @@
 const img_container = document.getElementById("imgs");
 const imgs = document.querySelectorAll(".image-container img");
+const prevButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
 
 let idx = 0;
 let interval = setInterval(run, 2000);
@@ -19,3 +21,20 @@ function changeImg() {
 
     img_container.style.transform = `translateX(${-idx * 500}px)`;
 }
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 4000);
+}
+
+prevButton.addEventListener('click', () => {
+    idx--;
+    changeImg();
+    resetInterval();
+})
+
+nextButton.addEventListener('click', () => {
+    idx++;
+    changeImg();
+    resetInterval();
+})
