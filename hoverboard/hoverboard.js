2f40e13b32b1c0db798c951aa404ecb3042a5b60
Aiman Nazmi <aimannazmi59@gmail.com>
2022-01-27 00:07:40 +0800

    add color

--- hoverboard/hoverboard.js
+++ hoverboard/hoverboard.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".container");
-
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const number = 503;
 
 for (let i = 0; i < number; i++) {
@@ -7,3 +7,4 @@ for (let i = 0; i < number; i++) {
     square.classList.add('square');
     container.appendChild(square);
 }
+
