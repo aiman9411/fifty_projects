e1fa7444b59339f1c6980ba6536fedc2954951b3
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-03 15:49:53 +0800

    edit searchItem function

--- live_user/live_user.js
+++ live_user/live_user.js
@@ -1,11 +1,15 @@
 const input = document.getElementById("input");
 const part2 = document.querySelector(".part2");
+const listItems = [];
+const button = document.getElementById("helo")
 
 getUser();
+input.addEventListener('input', (e) => searchItem(e.target.value));
 
 async function getUser() {
     const res = await fetch("https://randomuser.me/api/?results=50");
     const {results} = await res.json();
+    part2.innerHTML = '';
 
     results.forEach(item => {
         const user = document.createElement("div");
@@ -15,26 +19,21 @@ async function getUser() {
             <img src="${item.picture.large}" alt="User" class="image">
             <div class="info">
                 <h4 class="name">${item.name.first} ${item.name.last}</h4>
-                 <p class="location">${item.location.city}, ${item.location.state}</p>
+                 <p class="location">${item.location.state}, ${item.location.country}</p>
             </div>
         `
-
+        listItems.push(user);
         part2.appendChild(user);
     })
 
 }
 
-// input.addEventListener("input", (e) => {
-//     const users = document.querySelectorAll(".user");
-//     users.forEach(user => {
-//         if(e.target.value !== user.name.first) {
-//             user.classList.add("hide");
-//         }
-//     })
-// })
-
-
-function searchItem(term) {
-    const listUsers = document.querySelectorAll(".user");
-    
+function searchItem(searchTerm) {
+    listItems.forEach(item => {
+        if(!item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.add("active")
+        } else {
+            item.classList.remove("active");
+        }
+    })
 }
