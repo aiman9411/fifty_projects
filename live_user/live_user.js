8b4327aab0d8f4bbac3624f091a4702d2f1219cd
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-03 12:01:13 +0800

    add getUser function

---
+++ live_user/live_user.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("input");
+const part2 = document.querySelector(".part2");
+
+getUser();
+
+async function getUser() {
+    const res = await fetch("https://randomuser.me/api/?results=50");
+    const {results} = await res.json();
+
+    results.forEach(item => {
+        const user = document.createElement("div");
+        user.classList.add("user");
+        user.innerHTML = 
+        `
+            <img src="${item.picture.large}" alt="User" class="image">
+            <div class="info">
+                <h4 class="name">${item.name.first} ${item.name.last}</h4>
+                 <p class="location">${item.location.city}, ${item.location.state}</p>
+            </div>
+        `
+
+        part2.appendChild(user);
+    })
+
+}
+
