778c0d641068a41c570fce10c909b104dc6ed32a
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-13 15:55:21 +0800

    completed functions

---
+++ todo/todo.js
@@ -0,0 +1,70 @@
+const input = document.getElementById("input");
+const container = document.querySelector(".container");
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo));
+}
+
+input.addEventListener('keypress', (e) => {
+    if(e.key == "Enter" && input.value) {
+
+        addTodo();
+
+        input.value = "";
+        input.focus();
+
+        updateLS();
+
+    } else {
+        input.focus();
+    }
+})
+
+function addTodo(todo) {
+
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
+
+    if(todoText) {
+        const task = document.createElement("div");
+        task.classList.add("task");
+        task.innerHTML = todoText;
+        container.appendChild(task);
+
+        if(todo && todo.completed) {
+            task.classList.add("completed");
+        }
+        
+        task.addEventListener('click', () => {
+            task.classList.add('completed');
+            updateLS();
+        })
+
+        task.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            task.remove();
+            updateLS();
+        });
+    }
+};
+
+
+function updateLS() {
+    todosEl = document.querySelectorAll(".task");
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push( {
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed")
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
