e66349ae046b2dbdf11fb5a2f2094d0fef6e7887
Aiman Nazmi <aimannazmi59@gmail.com>
2022-02-15 10:28:34 +0800

    add functions

--- game/game.js
+++ game/game.js
@@ -1,1 +1,31 @@
 const screens = document.querySelectorAll(".screen");
+const selectInsectButtons = document.querySelectorAll(".select");
+const startButton = document.querySelector(".start");
+const timeEl = document.getElementById("time");
+const scoreEl = document.getElementById("score");
+const gameContainer = document.getElementById("game-container");
+const message = document.getElementById("message");
+let seconds = 0;
+let score = 0;
+let selectedInsects = [];
+
+startButton.addEventListener("click", () => screens[0].classList.add("up"));
+
+selectInsectButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img');
+        const src = img.getAttribute('src');
+        const alt = img.getAttribute('alt');
+        selectedInsects = { src, alt };
+        
+        screens[1].classList.add("up");
+        setTimeout(createInsect, 1000);
+        startGame();
+    })
+})
+
+function createInsect() {
+    const insect = document.createElement("div");
+    insect.classList.add("insect");
+    const { x, y } = getRandomLocation();
+}
